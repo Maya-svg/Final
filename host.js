41ea37f678d55bcdca509c3c0dd0b7cd29365b8d
Maya-svg <59680743+Maya-svg@users.noreply.github.com>
2021-12-12 22:57:09 -0500

    Update host.js

--- host.js
+++ host.js
@@ -17,19 +17,8 @@ http.createServer(function (req, res) {
   app.listen(port);
   console.log("Listening");
   
-  var q = url.parse(req.url, true);
- 
-  fs.readFile('scheduleView.html', function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-   res.write ("Success!  This app is deployed online");
-   res.end();
+  res.write ("Success!  This app is deployed online");
+  res.end();
 }).listen(port);
 
 
