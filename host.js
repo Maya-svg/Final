64925a88ea1659ed304d7e524c20ad42cb01e640
Maya-svg <59680743+Maya-svg@users.noreply.github.com>
2021-12-12 22:36:19 -0500

    Update host.js

--- host.js
+++ host.js
@@ -9,7 +9,15 @@ app.set('view engine','ejs');
 
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://Harnaljia:Harnaljia@band.huo1y.mongodb.net/Band?retryWrites=true&w=majority";
-var PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
+
+console.log("This goes to the console window");
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+   res.write("<h2>Hello World</h2>");
+   res.write ("Success!  This app is deployed online");
+   res.end();
+}).listen(port);
 
 // app.listen(process.env.PORT || 3000, function(){
 //   console.log("listening");
