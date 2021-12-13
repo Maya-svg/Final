ca54173e17ab2a4ec813312c65e3ed03e9c2c9fc
Maya-svg <59680743+Maya-svg@users.noreply.github.com>
2021-12-12 22:46:17 -0500

    Update host.js

--- host.js
+++ host.js
@@ -18,9 +18,23 @@ http.createServer(function (req, res) {
     console.log('Listening');
   });
   
+  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db){
+    if(err) { return console.log(err); return;}
+    
+    var dbo = db.db("Info");
+    var collection = dbo.collection('schedule');
+
+    console.log("connected to Mongo");
+
+    app.get('/',function(req,res){
+        console.log("In get request");
+        populate(collection,res);
+        res.write ("inside get function");
+    }); 
+   });
+  
    res.write ("Success!  This app is deployed online");
    res.end();
-  
 }).listen(port);
 
 
