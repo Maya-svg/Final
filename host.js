5c80212cbfb28942a6c3fbc0dd8402586a5e0966
Maya-svg <59680743+Maya-svg@users.noreply.github.com>
2021-12-12 23:09:03 -0500

    Update host.js

--- host.js
+++ host.js
@@ -4,6 +4,7 @@ var qs = require('querystring');
 
 const express = require('express');
 const app = express();
+app.use(express.static("public"));
 const bodyParser = require('body-parser');
 app.set('view engine','ejs');
 
@@ -11,22 +12,9 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb+srv://Harnaljia:Harnaljia@band.huo1y.mongodb.net/Band?retryWrites=true&w=majority";
 var port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-  console.log("Server created");
-  
-  app.listen(port);
-  console.log("Listening");
-  
-  fs.readFile('./scheduleView.html', function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("INSIDE");
-    return res.end();
-  });
-}).listen(port);
+app.get('/',function(req,res){
+    res.send("Hi");
+});
 
-
-
+app.listen(port, 
+	() => console.log("Server is running..."));
